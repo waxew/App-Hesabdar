8078fb13c86462b0b7b652bc0a4bd3850bd16352
waxew <4622.ar@gmail.com>
2026-08-29 02:30:57 +0330

    chore: bump App-Hesabdar to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.waxew.hesabdar"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
