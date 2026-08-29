1a7e8c51de83d7f5263d2975622db4f70823453a
waxew <4622.ar@gmail.com>
2026-08-29 13:09:46 +0330

    chore: bump App-Hesabdar to 0.11.0 beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.waxew.hesabdar"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.10.0"
+        versionCode = 6
+        versionName = "0.11.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
